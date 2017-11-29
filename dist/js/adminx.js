8f8d9ac18049d9a40f5eb830fae37cf01c975fd7
Max Mitschke <max.mitschke@icloud.com>
2017-11-29 20:44:43 +0100

    first work on tables

--- dist/js/adminx.js
+++ dist/js/adminx.js
@@ -91,6 +91,35 @@ $(document).ready(function () {
     e.preventDefault();
     $('.adminx-sidebar').toggleClass('in');
   });
+
+  // Highlight row when selected
+  $('tr .table-select-row').change(function () {
+    // this will contain a reference to the checkbox   
+    if (this.checked) {
+      $(this).closest('tr').addClass('selected');
+    } else {
+      $(this).closest('tr').removeClass('selected');
+
+      var selectAll = $(this).closest('table').find('.table-select-all');
+
+      if (selectAll.hasClass('all-selected')) {
+        selectAll.removeClass('all-selected');
+        selectAll.prop('checked', false);
+      }
+    }
+  });
+
+  // check all checkboxes in table
+  $('tr .table-select-all').change(function () {
+    // this will contain a reference to the checkbox   
+    if (this.checked) {
+      $(this).closest('table').find('.table-select-row').prop('checked', true).change();
+      $(this).addClass('all-selected');
+    } else {
+      $(this).closest('table').find('.table-select-row').prop('checked', false).change();
+      $(this).removeClass('all-selected');
+    }
+  });
 });
 
 /***/ }),
