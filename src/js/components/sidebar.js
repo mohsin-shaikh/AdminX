dae180b7a86e5ae3ac82f1372c7702688d116284
Max Mitschke <max.mitschke@icloud.com>
2017-12-06 22:32:01 +0100

    fix sidebar bug

--- src/js/components/sidebar.js
+++ src/js/components/sidebar.js
@@ -1,10 +1,17 @@
+import { hasClass, removeClass, addClass } from '../util/utilities';
+
 document.addEventListener("DOMContentLoaded", function() {
   // Sidebar toggle
   const toggleElements = document.querySelectorAll('.sidebar-toggle');
 
   Array.from(toggleElements).forEach(element => {
     element.addEventListener('click', event => {
-      document.querySelectorAll('.adminx-sidebar')[0].classList.toggle('in');
+      const sidebar = document.getElementById('#sidebar');
+      if (hasClass(sidebar, 'in')) {
+        removeClass(sidebar, 'in');
+      } else {
+        addClass(sidebar, 'in');
+      }
     });
   });
 });
